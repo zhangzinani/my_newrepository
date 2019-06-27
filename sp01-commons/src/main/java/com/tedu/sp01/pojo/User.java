2ba577bc163566a36200c3ecb3cd77b3c6da6a77
张子楠 <@LAPTOP-4PSN6VEO>
2019-06-27 17:13:59 +0800

    1.commons上传

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/User.java
@@ -0,0 +1,17 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class User {
+
+	private Integer id;
+	private String username;
+	private String password;
+}
